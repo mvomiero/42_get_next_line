138e5484b8bdedb423081fd81f080d80a3e311ca
Marco Vomiero <colocomarco1@gmail.com>
2022-12-26 19:24:05 +0100

    main file code finished

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,73 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 15:50:32 by mvomiero          #+#    #+#             */
-/*   Updated: 2022/12/26 15:55:28 by mvomiero         ###   ########.fr       */
+/*   Updated: 2022/12/26 19:22:11 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*extract_stash(int fd, char *buf, char *stash)
+{
+	size_t	read_value;
+	char	*char_temp;
+
+	read_value = 1;
+	while (read_value != 0)
+	{
+		read_value = read(fd, buf, BUFFER_SIZE + 1);
+		if (read_value == -1)
+			return (NULL);
+		else if (read_value == 0)
+			break ;
+		buf[read_value] = '\0';
+		if (!stash)
+			stash = ft_strdup("");
+		char_temp = stash;
+		stash = ft_strjoin(char_temp, buf);
+		free(char_temp);
+		char_temp = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+static char	*extract_line(char *line)
+{
+	size_t	pos;
+	char	*stash;
+
+	pos = 0;
+	while (line[pos] != '\0' || line[pos] != '\n')
+		pos++;
+	if (line[pos] == '\0' || line [1] == '\n' || line[pos + 1] == '\0')
+		return (0);
+	stash = ft_substr(line, pos + 1, ft_strlen(line) - pos);
+	line[pos + 1] = '\0';
+	return (stash);
+}
+
 char *get_next_line(int fd)
 {
-	char	*stash;
-	char	*buf;
+	static char	*stash;
+	char		*buf;
+	char		*line;
 
-
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = extract_stash(fd, buf, stash);
+	free(buf);
+	buf = NULL;
+	if (!line)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	stash = extract_line(line);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,26 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 15:51:04 by mvomiero          #+#    #+#             */
-/*   Updated: 2022/12/26 15:55:28 by mvomiero         ###   ########.fr       */
+/*   Updated: 2022/12/26 19:23:52 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-// Headers for malloc, free and read
+// Headers for functions: malloc, free, read
 # include <stdlib.h>
 # include <unistd.h>
+
+static char	*extract_stash(int fd, char *buf, char *stash);
+static char	*extract_line(char *line);
+char *get_next_line(int fd);
+
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 5
+# endif
+
 
 
 
