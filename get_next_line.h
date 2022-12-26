1538514781e4fe41b6e713ed21d7f2d4155d6f7c
Marco Vomiero <colocomarco1@gmail.com>
2022-12-26 15:55:46 +0100

    setting the files

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/26 15:51:04 by mvomiero          #+#    #+#             */
+/*   Updated: 2022/12/26 15:55:28 by mvomiero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// Headers for malloc, free and read
+# include <stdlib.h>
+# include <unistd.h>
+
+
+
+
+#endif
